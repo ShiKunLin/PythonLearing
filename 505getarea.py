08f78fbbe7bc9956b58a9c03bc7f9ccd63924840
ShiKunLin <C109156246@nkust.edu.tw>
2021-05-05 15:22:06 +0800

    123

--- 505getarea.py
+++ 505getarea.py
@@ -10,6 +10,23 @@ class Circle:
 
 C1 =Circle(10)
 print(C1.get_area())
+
 C2 =Circle(20)
 print(C2.get_area())
+
+
+class Sidelength:
+    def __init__(self,l=0):
+        self.l=l
+    
+    def get_area(self):
+        return self.l*self.l
+
+    def get_metar(self):
+        return self.l*4
+
+
+R1 =Sidelength(6)
+print("長",R1.l," ","面積為",R1.get_area()," ","邊長為",R1.get_metar())
+
 
